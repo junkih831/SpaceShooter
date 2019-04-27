d4c60eef3dd5451854629cbdc8a7633566677f7c
홍준기 <junkihongki@gmail.com>
2019-04-27 23:26:40 +0900

    재장전 기능 구현 - 레이어 분리

--- Assets/02.Scripts/Enemy/EnemyFire.cs
+++ Assets/02.Scripts/Enemy/EnemyFire.cs
@@ -15,6 +15,7 @@ public class EnemyFire : MonoBehaviour
 
     // 애니메이터 컨트롤러에 정의한 파라미터의 해시값을 미리 추출
     private readonly int hashFire = Animator.StringToHash("Fire");
+    private readonly int hasReload = Animator.StringToHash("Reload");
 
     // 다음 발사할 시간 계산용 변수
     private float nextFire = 0.0f;
@@ -23,10 +24,20 @@ public class EnemyFire : MonoBehaviour
     // 주인공을 향해 회전할 속도 계수
     private readonly float damping = 10.0f;
 
+    private readonly float reloadTime = 2.0f;   // 재장전 시간
+    private readonly int maxBullet = 10;        // 탄창의 최대 총알 수
+    private int currBullet = 10;                // 초기 총알 수
+    private bool isReload = false;              // 재장전 여부
+    // 재장전 시간 동안 기다릴 변수 선언
+    private WaitForSeconds wsReload;
+
     // 총알 발사 여부를 판단할 변수
     public bool isFire = false;
     // 총알 발사 사운드를 저장할 변수
     public AudioClip fireSfx;
+    // 재장전 사운드를 저장할 변수
+    public AudioClip reloadSfx;
+
 
     // Start is called before the first frame update
     void Start()
@@ -37,12 +48,14 @@ public class EnemyFire : MonoBehaviour
         animator = GetComponent<Animator>();
         audio = GetComponent<AudioSource>();
 
+        wsReload = new WaitForSeconds(reloadTime);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(isFire)
+        if(!isReload && isFire)
         {
             // 현재 시간이 다음 발사 시간보다 큰지를 확인
             if(Time.time >= nextFire)
@@ -64,5 +77,27 @@ public class EnemyFire : MonoBehaviour
     {
         animator.SetTrigger(hashFire);
         audio.PlayOneShot(fireSfx, 1.0f);
+
+        // 남은 총알로 재장전 여부를 계산
+        isReload = (--currBullet % maxBullet == 0);
+        if(isReload)
+        {
+            StartCoroutine(Reloading());
+        }
+    }
+
+    IEnumerator Reloading()
+    {
+        // 재장전 애니매이션 실행
+        animator.SetTrigger(hasReload);
+        // 재장전 사운드 발생
+        audio.PlayOneShot(reloadSfx, 1.0f);
+
+        // 재장전 시간만큼 대기하는 동안 제어권 양보
+        yield return wsReload;
+
+        // 총알의 개수를 초기화
+        currBullet = maxBullet;
+        isReload = false;
     }
 }
