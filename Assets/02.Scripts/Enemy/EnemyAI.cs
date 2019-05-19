061c248d461af3e990f0d1a79765a48422007bfa
홍준기 <junkihongki@gmail.com>
2019-05-19 17:40:22 +0900

    불규칙적인 순찰 메시지전달 - SendMessage 사용자 정의 이벤트 - Delegate

--- Assets/02.Scripts/Enemy/EnemyAI.cs
+++ Assets/02.Scripts/Enemy/EnemyAI.cs
@@ -45,6 +45,7 @@ public class EnemyAI : MonoBehaviour
     private readonly int hashDieIdx = Animator.StringToHash("DieIdx");
     private readonly int hashOffset = Animator.StringToHash("Offset");
     private readonly int hashWalkSpeed = Animator.StringToHash("WalkSpeed");
+    private readonly int hashPlayerDie = Animator.StringToHash("PlayerDie");
 
 
     private void Awake()
@@ -78,6 +79,14 @@ public class EnemyAI : MonoBehaviour
         StartCoroutine(CheckState());
         // Action 코루틴 함수 실행
         StartCoroutine(Action());
+
+        Damage.OnPlayerDie += this.OnPlayerDie;
+    }
+
+    void OnDisable()
+    {
+        Damage.OnPlayerDie -= this.OnPlayerDie;
+        
     }
 
     // 적 캐릭터의 상태를 검사하는 코루틴 함수
@@ -166,6 +175,16 @@ public class EnemyAI : MonoBehaviour
     {
         // Speed 파라미터에 이동 속도를 전달
         animator.SetFloat(hashSpeed, moveAgent.speed);
+    }
+
+    public void OnPlayerDie()
+    {
+        moveAgent.Stop();
+        enemyFire.isFire = false;
+        // 모든 코루틴 함수를 종료시킴
+        StopAllCoroutines();
+
+        animator.SetTrigger(hashPlayerDie);
     }
 
 }
